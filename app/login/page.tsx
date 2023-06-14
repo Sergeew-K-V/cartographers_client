8e1d2619dc3cd6d8033811cbcca42a4423ef60e0
kirill.sergeev <kirill.sergeev@progforce.com>
2023-06-14 14:29:52 +0300

    refactor: login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/features/Login';
 
 const LoginPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-secondary-50">
+    <main className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <LoginForm />
     </main>
   );
