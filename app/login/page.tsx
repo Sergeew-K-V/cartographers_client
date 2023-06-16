9ff80fdb016cb57c24d2e4b0e9fbe792ca97657d
kirill.sergeev <kirill.sergeev@progforce.com>
2023-06-16 12:13:32 +0300

    refactor: colors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/features/Login';
 
 const LoginPage = () => {
   return (
-    <main className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+    <main className="flex items-center min-h-screen p-6 bg-secondary-50">
       <LoginForm />
     </main>
   );
