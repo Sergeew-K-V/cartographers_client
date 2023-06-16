900d644b424fac3e59b818dde96222eaeb6188cf
kirill.sergeev <kirill.sergeev@progforce.com>
2023-06-16 10:19:06 +0300

    feat: added loader

--- shared/ui/index.ts
+++ shared/ui/index.ts
@@ -2,5 +2,6 @@ import Button from './Button';
 import Checkbox from './Checkbox';
 import Input from './Input';
 import LinkButton from './LinkButton';
+import Loader from './Loader';
 
-export { Input, Button, LinkButton, Checkbox };
+export { Input, Button, LinkButton, Checkbox, Loader };
