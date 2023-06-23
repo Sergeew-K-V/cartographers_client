8142b27e820ba09626f00d0d0ce93b06710e8631
kirill.sergeev <kirill.sergeev@progforce.com>
2023-06-23 14:22:04 +0300

    feat: added divider for coins

--- app/hub/playground/page.tsx
+++ app/hub/playground/page.tsx
@@ -3,13 +3,15 @@
 import { AlertList, Button, ImageCustom } from '@/shared/ui';
 import { grid } from './config';
 
+const classDivider = 'scale-[2] w-full flex justify-center';
+
 function PlaygroundPage(): JSX.Element {
   return (
     <main className="min-h-[calc(100vh-64px)] p-6 bg-secondary-50">
       <div className="container min-w-full relative">
         <div className="grid grid-cols-3 w-full justify-items-center">
           <div className="grid grid-rows-4 max-h-fit">
-            <div className="row-start-2">
+            <div className="row-start-3">
               <div className="mb-2">Game stats</div>
               {/* <div className="grid grid-cols-2 gap-4">
                 <div className="border-2 border-secondary-900 bg-secondary-50 min-w-fit shadow-2xl">
@@ -47,13 +49,13 @@ function PlaygroundPage(): JSX.Element {
                     <th
                       scope="row"
                       className={
-                        'px-6 py-4 font-medium text-secondary-900 whitespace-nowrap'
+                        'px-6 py-4 whitespace-nowrap font-bold text-primary-700'
                       }
                     >
-                      You
+                      Kirill
                     </th>
                     <td className="px-6 py-4">No</td>
-                    <td className="px-6 py-4">1230123</td>
+                    <td className="px-6 py-4">120</td>
                   </tr>
                   <tr className={'border-b text-center'}>
                     <th
@@ -106,32 +108,67 @@ function PlaygroundPage(): JSX.Element {
                   ))
                 )}
               </div>
+              <div className="absolute top-[50px] left-[37px] z-10 uppercase whitespace-nowrap font-bold text-primary-700">
+                Kirill
+              </div>
+              <div className="absolute top-[110px] left-[37px] z-10 uppercase whitespace-nowrap font-bold text-primary-700">
+                Legendary
+              </div>
+              <div className="absolute bottom-[110px] w-[480px] h-[30px] left-[90px] z-10 flex justify-between">
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+                <div className={classDivider}>/</div>
+              </div>
             </div>
           </div>
           <div className="grid grid-cols-1 gap-y-4 h-full">
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col gap-y-2 justify-center items-center">
               <span className="uppercase font-bold text-xl">Current card</span>
               <ImageCustom
                 src="/images/cards/card_1.png"
-                width={200}
+                width={600}
+                className="w-52"
                 alt="Current card"
               />
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <span className="font-bold">Previous card</span>
-              <ImageCustom
-                src="/images/cards/card_3.png"
-                width={200}
-                className="w-32 hover:animate-wiggle"
-                alt="Previous card"
-              />
+            <div className="grid grid-cols-2">
+              <div className="flex flex-col gap-y-2 justify-center items-center">
+                <span className="font-bold">Previous card</span>
+                <ImageCustom
+                  src="/images/cards/card_3.png"
+                  width={200}
+                  className="w-32 hover:animate-wiggle"
+                  alt="Previous card"
+                />
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center">
+                <span className="font-bold">
+                  Remaining cards in the deck 6/13
+                </span>
+                <ImageCustom
+                  src="/images/other/explore_back.jpg"
+                  width={600}
+                  className="w-32"
+                  alt="Previous card"
+                />
+              </div>
             </div>
             <div className="flex gap-x-4 items-center">
-              <div className="w-40">
-                <Button>Submit</Button>
+              <div className="w-32">
+                <Button>Submit step</Button>
               </div>
-              <div className="w-40">
-                <Button>Reset</Button>
+              <div className="w-32">
+                <Button>Reset step</Button>
               </div>
             </div>
           </div>
