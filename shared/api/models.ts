8787dee9076b2da3c6c80cc686b26eb115292c64
kirill.sergeev <kirill.sergeev@progforce.com>
2023-06-28 14:56:22 +0300

    refactor: update IUser

--- shared/api/models.ts
+++ shared/api/models.ts
@@ -1,6 +1,14 @@
 interface IUser {
   email: string;
+  _id?: string;
   password: string;
+  nickname?: string;
+  rang?: string;
+  gameStats?: {
+    rate: number;
+    wins: number;
+    loses: number;
+  };
 }
 
 interface GameSessionInfo {
