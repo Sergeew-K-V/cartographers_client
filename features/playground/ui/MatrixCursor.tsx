dd0d671386e8ae7630024f99ac2fddcb13295193
kirill.sergeev <kirill.sergeev@progforce.com>
2023-08-29 15:07:57 +0300

    refactor: matrix cursor

--- features/playground/ui/MatrixCursor.tsx
+++ features/playground/ui/MatrixCursor.tsx
@@ -30,55 +30,32 @@ const MatrixCursor = ({
     if (!canvas) {
       return;
     }
+
     const ctx = canvas.getContext('2d');
     if (!ctx) {
       return;
     }
 
-    const imagePromises = [];
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
         const value = matrix[row][col];
+
         if (value === 1) {
-          const imagePromise = new Promise((resolve, reject) => {
-            const image = new Image();
-            image.src = `/images/type_card/${selectedType}.png`;
-            image.onload = () => resolve(image);
-            image.onerror = () =>
-              reject(new Error(`Failed to load image: ${image.src}`));
-          });
-          imagePromises.push(imagePromise);
+          const image = new Image();
+          image.src = `/images/type_card/${selectedType}.png`;
+          image.onload = () =>
+            ctx.drawImage(
+              image,
+              col * cellSize,
+              row * cellSize,
+              cellSize,
+              cellSize
+            );
         } else {
-          const imagePromise = new Promise((resolve, reject) => {
-            const image = new Image();
-            image.src = `/images/type_card/cell.png`;
-            image.style.display = 'none';
-            image.onload = () => resolve(image);
-            image.onerror = () =>
-              reject(new Error(`Failed to load image: ${image.src}`));
-          });
-          imagePromises.push(imagePromise);
+          ctx.strokeRect(col * cellSize, row * cellSize, cellSize, cellSize);
         }
       }
     }
-
-    Promise.all(imagePromises)
-      .then((loadedImages) => {
-        loadedImages.forEach((image, index) => {
-          const row = Math.floor(index / cols);
-          const col = index % cols;
-          ctx.drawImage(
-            image as CanvasImageSource,
-            col * cellSize,
-            row * cellSize,
-            cellSize,
-            cellSize
-          );
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   }, [matrix, rows, cols, cellSize, selectedType]);
 
   return (
@@ -91,7 +68,7 @@ const MatrixCursor = ({
         left: position.x - cellSize,
       }}
       className={
-        'absolute opacity-50 z-10 pointer-events-none ' +
+        'absolute opacity-80 z-10 pointer-events-none ' +
         (isVisibleMatrixCursor ? 'cursor-none' : '')
       }
       onMouseMove={changeCursorPositionHandler}
